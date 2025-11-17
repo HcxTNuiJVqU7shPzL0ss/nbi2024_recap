2cc39161f51cff4a33868fd52a264e839e7b78f1
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2025-11-17 17:09:43 +0100

    Adding week 1 ex 2
    Files for week 1 exercise 2 added

---
+++ l02_prog_m_py/w01/week1_assignment/ex_02/README.md
@@ -0,0 +1,21 @@
+Correct any errors.
+
+What does the code do?
+
+Come up with better names than w, y and z.
+
+---
+
+ x = 100 # biljettpris
+
+ y = 200 # pengar på fickan
+
+ print("Det blir " + (y- x) " kronor över.")
+
+ z = 200- 100 / 2
+
+ print("Hälften är: " + z)
+
+---
+
+EOF
---
+++ l02_prog_m_py/w01/week1_assignment/ex_02/discuss.py
@@ -0,0 +1,42 @@
+"""Module for 'Discuss'."""
+
+
+#####################################################################
+# Copyright 2025 gnoff
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#####################################################################
+
+
+def press_exit():
+    """Use to prompt user to hit enter to continue."""
+    input('Press Return (Enter) to exit.\n')
+
+
+TICKET_PRICE = 100 # Ticket price
+FUNDS = 200 # Available funds
+
+
+# Calculate how much you will have left
+MONEY_LEFT = FUNDS - TICKET_PRICE
+
+# Calculate half of what is left
+HALF_LEFT = MONEY_LEFT / 2
+
+
+print('\nThere is', MONEY_LEFT, "SEK over.")
+print('Half of what is left over is:', HALF_LEFT, '\n')
+
+
+press_exit()
