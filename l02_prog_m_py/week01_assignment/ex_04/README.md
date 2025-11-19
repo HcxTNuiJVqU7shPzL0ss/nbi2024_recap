0735928d3ba9649ea335c8b95e786948dd24f332
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2025-11-19 02:00:10 +0100

    Adding exc 4 parts 1a and 1b
    New exc 1a and 1b added

---
+++ l02_prog_m_py/week01_assignment/ex_04/README.md
@@ -0,0 +1,35 @@
+# 3 More exercises
+A bit more advanced level.
+
+---
+
+## 1a
+It is about 470 km between Stockholm and 
+Gothenburg.\
+Write a program that calculates how long it will 
+take to drive from Stockholm to Gothenburg.\
+You need to ask how fast the user wants to drive, 
+in km/h.
+
+---
+
+## 1b
+Rewrite the program so that it answers in minutes, 
+instead of seconds.
+
+---
+
+## 1c
+More difficult.\
+Now the program shall answer in whole hours 
+and minutes.
+
+---
+
+## 2
+Write a program that calculate the hypotenuse in a 
+right triangle.\
+The user need to enter the length of the two shorter 
+sides.
+
+---
---
+++ l02_prog_m_py/week01_assignment/ex_04/ex04_01b.py
@@ -0,0 +1,43 @@
+"""Module for ex04, part 1b."""
+
+
+#####################################################################
+# Copyright 2025 gnoff
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#####################################################################
+
+
+from my_funct_dir.my_base_functions import (press_continue,
+                                            press_exit,
+                                            enter_int)
+
+
+print('\nExercise 4, part 1b.\n')
+press_continue()
+
+
+DISTANCE = 470 # Nr of km between sthlm and gbg
+IN_STRING = 'How fast, in km/h, do you want to drive: '
+
+speed_kmh = enter_int(IN_STRING, 50, 120, True)
+
+speed_mps = (speed_kmh * 1000) / 3600
+
+nr_of_sec = (DISTANCE * 1000) / speed_mps
+
+nr_of_min = int(nr_of_sec / 60)
+
+print('\nIt will take you ~', nr_of_min, 'minutes to drive.')
+press_exit()
