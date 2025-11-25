7f0a655a12c0c6eb08fc77f336f87a31f5f274c2
rock_ <rock_gnoff@hotmail.com>
2025-11-25 15:48:02 +0100

    Play with IDE vs cmd
    Adding setup and some basic
    test files
    Also, built using setuptools,
    needs pip install it seems

---
+++ l02_prog_m_py/remove_later.py
@@ -0,0 +1,8 @@
+import sys
+
+from my_funct_dir.my_base_functions import press_exit
+
+print('Version: ', sys.version)
+print('Executable: ', sys.executable)
+
+press_exit()
---
+++ setup.py
@@ -0,0 +1,13 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+
+from setuptools import setup, find_packages
+#import pathlib
+
+
+setup(name='l02', version='0.0.1', packages=find_packages())
