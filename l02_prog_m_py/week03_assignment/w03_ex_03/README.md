9c2370dbad87f489da3dbadae5a0a9e0baae30e2
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2025-11-28 06:04:57 +0100

    Add L2, W3, E3, first
    Adding version 1
    and version 2
    so far

---
+++ l02_prog_m_py/week03_assignment/w03_ex_03/README.md
@@ -0,0 +1,97 @@
+
+# Week 03, Exercise 03
+
+---
+
+## 3 Kvittouträknaren
+
+---
+
+### Version 1
+Gör ett program som upprepade gånger ber användaren skriva in ett tal.\
+När man skriver in strängen "quit" eller "avsluta" ska programmet ska 
+det räkna ut summan av talen.\
+Exempel:
+
+
+    Välkommen till Kvittokompis! Avsluta genom att skriva: quit 
+    Skriv in ett belopp: 25
+    Skriv in ett belopp: 50
+    Skriv in ett belopp: quit 
+    Det blir 75 kr totalt. Välkommen åter!
+
+Tips! För att lösa uppgiften behöver du:\
+flera variabler, input, while-loop
+
+---
+
+### Version 2
+Programmet ska fråga hur många man är, och tala om hur mycket varje 
+person i sällskapet ska betala.
+
+    Hur många är ni? 3
+    Det blir 75 kr totalt, alltså 25.0 kr per person. Välkommen åter!
+
+---
+
+### Version 3
+Programmet ska fråga hur många procent dricks man vill lägga på.\
+Om användaren inte skriver något (tom sträng) ska programmet använda 
+10% som standardinställning. 
+
+---
+
+### Version 4
+
+Nice to have:\
+Prova att använda try+except eller isdigit för att hantera de fall 
+när användaren skriver ett felaktigt värde.\
+
+    https://www.w3schools.com/python/python_try_except.asp
+
+    https://stackoverflow.com/questions/354038/how-do-i-check-if-a-string-represents-a-number-float-or-int
+
+("Nice to have" handlar om funktionalitet som inte är nödvändig, 
+men som är bra att ha.\
+Gå gärna vidare till nästa uppgift och återvänd till den här 
+om du vill lära dig mer.)
+
+---
+
+### Testa programmet med följande inputs: 
+**Version 1:**\
+100\
+10, 20, 15\
+
+
+**Version 2:**\
+100, 1 person\
+100, 2 personer\
+10, 10, 40 personer\
+30, 20, 30, 1 person\
+
+
+Lägg till egna testfall för dricksen.
+
+---
+
+Student == gnoff
+
+---
+
+Copyright 2025 gnoff
+
+Licensed under the Apache License, Version 2.0 (the "License");\
+you may not use this file except in compliance with the License.\
+You may obtain a copy of the License at\
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software\
+distributed under the License is distributed on an "AS IS" BASIS,\
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or\
+implied.\
+See the License for the specific language governing permissions and\
+limitations under the License.\
+
+---
---
+++ l02_prog_m_py/week03_assignment/w03_ex_03/ex03_03_receipt_calc.py
@@ -0,0 +1,115 @@
+"""Module for Lesson 02, Week 03, Exercise 03.
+
+Receipt Calculator.
+"""
+
+#####################################################################
+# Copyright 2025 gnoff
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#####################################################################
+
+
+import sys
+
+
+from my_funct_dir.my_base_functions import (press_continue,
+                                            press_exit,
+                                            press_goback, enter_int)
+
+
+def w3_ex3_user_input():
+    """Use to get user input for the calculator."""
+    end_it_clean = ['quit', 'avsluta', 'exit', 'done', 'stop']
+    end_it_parens = ['quit()', 'avsluta()', 'exit()', 'done()',
+                     'stop()']
+    int_list = []
+    print('Ex 03 User Input.\nFunction: ',
+          w3_ex3_user_input.__name__, sep='')
+    print('\nYou will be asked to input integers.\n'
+          'Keep doing this until you want to stop, '
+          'then instead type "quit".')
+    press_continue()
+
+    print('Welcome to Receipt Buddy (TM)!\n'
+          'Exit by enter "quit".\n')
+
+    while True:
+        str_in = input('Enter a value:\n')
+        if (str_in in end_it_clean) or (str_in in end_it_parens):
+            if sum(int_list) == 0:
+                print('\nNo values entered, exit hard.')
+                sys.exit()
+            return sum(int_list)
+        try:
+            int_in = int(str_in)
+            if int_in > 0:
+                int_list.append(int_in)
+                print('\nValue ' + str(int_in) + ' added.')
+                press_continue()
+            else:
+                print('\nPlease use a positive integer!')
+                press_goback()
+            continue
+        except ValueError:
+            print('\nPlease use an integer!')
+            press_goback()
+            continue
+
+
+def w3_ex3_no_ppl():
+    """Use to input how many people should split the bill."""
+    print('Ex 03 People.\nFunction: ',
+          w3_ex3_no_ppl.__name__, sep='')
+    print('\nYou will be asked to input an integer.\n'
+          'This is to represent how many of you there are that shall '
+          'split the bill.')
+    press_continue()
+    in_string = 'How many should split the bill: '
+    no_of_ppl = enter_int(in_string)
+    return no_of_ppl
+
+
+def main():
+    """Use as main function."""
+    print('\nWeek 03, Exercise 03, Receipt Calculator.\nFunction: ',
+          main.__name__, sep = '')
+    press_continue()
+
+    # Version 1
+    res_v1 = w3_ex3_user_input()
+
+
+    # Version 2
+    ppl_v2 = w3_ex3_no_ppl()
+    if ppl_v2 < 1:
+        print('\nNeed to protect from div by 0!\n'
+              'Hard Exit!')
+        sys.exit()
+    else:
+        shared = res_v1 / ppl_v2
+
+
+    # Printouts
+    print('\nVersion 1:\n'
+          'The total is: ' + str(res_v1) + ' SEK. Please come back!')
+    print('\nVersion 2:\n'
+          '\nYou should pay ~' + f'{shared:.2f}' +
+          ' SEK per person.')
+
+    press_exit()
+
+
+if __name__ == "__main__":
+    main()
