d42d2ab2a5b0bf20ffdcc0f883d2914d22258988
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2025-11-28 06:49:20 +0100

    L2, W3, E3, final
    Version 3 added
    Also final calculations
    More nice to have
    Feel done for now

--- l02_prog_m_py/week03_assignment/w03_ex_03/ex03_03_receipt_calc.py
+++ l02_prog_m_py/week03_assignment/w03_ex_03/ex03_03_receipt_calc.py
@@ -76,9 +76,57 @@ def w3_ex3_no_ppl():
           'This is to represent how many of you there are that shall '
           'split the bill.')
     press_continue()
-    in_string = 'How many should split the bill: '
-    no_of_ppl = enter_int(in_string)
+    in_string_ppl = 'How many should split the bill: '
+    no_of_ppl = enter_int(in_string_ppl)
+    print('\n')
     return no_of_ppl
+
+
+def w3_ex3_tips():
+    """Use to enter how much tips to leave.
+
+    If no entry, 10% is standard.
+    """
+    print('Ex 03 Tips.\nFunction: ',
+          w3_ex3_tips.__name__, sep='')
+    print('\nYou will be asked to input an integer.\n'
+          'This is to represent how tips to leave.\n'
+          'If blank, there will be 10% added!')
+    press_continue()
+    while True:
+        s_tip = input('How much tip (in %) to leave: ')
+        if len(s_tip) == 0:
+            i_tip = 10
+            return i_tip
+        try:
+            i_tip = int(s_tip)
+            if i_tip < 0:
+                print('\nNegative not allowed!'
+                      'Crash!')
+                sys.exit()
+            if i_tip == 0:
+                print('\nReally, 0%?')
+            elif i_tip > 20:
+                print('\nNow we are generous!')
+            return i_tip
+        except ValueError:
+            print('\nPlease try again!')
+            press_goback()
+            continue
+
+
+def w3_ex3_new_calc(the_sum, the_ppl, the_tips):
+    """Use to calculate it all."""
+    print('Ex 03 Final Calculation.\nFunction: ',
+          w3_ex3_new_calc.__name__, sep='')
+    print('\nThis is the final calculation.')
+    press_continue()
+    print('The sum total was: ' + str(the_sum) + ' SEK.')
+    print('The number of people was: ' + str(the_ppl) + '.')
+    print('The tips to leave was: ' + str(the_tips) + '%.')
+    new_sum = the_sum + (the_sum * (the_tips/100))
+    per_person = new_sum / the_ppl
+    print('Each person shall pay ~' + f'{per_person:.2f}' + ' SEK.')
 
 
 def main():
@@ -102,12 +150,24 @@ def main():
         shared = res_v1 / ppl_v2
 
 
+    # Version 3
+    the_tip_v3 = w3_ex3_tips()
+
+
     # Printouts
+    press_continue()
     print('\nVersion 1:\n'
           'The total is: ' + str(res_v1) + ' SEK. Please come back!')
     print('\nVersion 2:\n'
           'You should pay ~' + f'{shared:.2f}' +
           ' SEK per person.')
+    print('\nVersion 3:\n'
+          'The tip is: ' + str(the_tip_v3) + '%')
+    press_continue()
+
+    # Final calculations
+    w3_ex3_new_calc(res_v1, ppl_v2, the_tip_v3)
+
 
     press_exit()
 
