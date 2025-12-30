1a02bc569ff300598ba635a7a3f4e06c5cb1fcd7
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2025-12-30 15:17:34 +0100

    Cleanup ex02
    Remove unused code
    Can be seen in previous
    versions

--- l02_prog_m_py/week04_assignment/w04_ex_02/ex02_functions.py
+++ l02_prog_m_py/week04_assignment/w04_ex_02/ex02_functions.py
@@ -26,11 +26,8 @@ from my_funct_dir.my_base_functions import (press_continue,
 
 def my_function_2_1(in_name):
     """Use for exercise 02, part 01."""
-    # print('\nThis is exercise 01.\n')
     add_string = ' is a real hacker'
     print('\n' + in_name + add_string)
-    # print('\nEnd of part 01.')
-    # press_continue()
 
 
 def ex01():
@@ -45,10 +42,7 @@ def ex01():
 
 def eko_2a(in_string):
     """Use to echo incoming string."""
-    # print('\nThis is exercise 02a.\n')
     print(in_string + in_string)
-    # print('\nEnd of part 02a.')
-    # press_continue()
 
 
 def ex02a():
@@ -62,17 +56,7 @@ def ex02a():
 
 def eko_2b(in_string, count):
     """Use to echo incoming string count number of times."""
-    # print('\nThis is exercise 02b.\n')
-    # int_cnt = count
-    # int_string = in_string
     print('\n' + in_string * count)
-    # int_cnt -= 1
-    # while int_cnt > 0:
-    #     int_string += in_string
-    #     int_cnt -= 1
-    # print(int_string)
-    # print('\nEnd of part 02b.')
-    # press_continue()
 
 
 def ex02b():
@@ -89,8 +73,6 @@ def ex02b():
 def my_function_2_3():
     """Use for exercise 02, part 03."""
     # Loop 5 times, should print out: 32.
-    # print('\nThis is exercise 03.\n')
-
     end = 5
     y = 1
     for x in range(1, 100):
@@ -99,9 +81,6 @@ def my_function_2_3():
         if x == end:
             break
     print(y)
-
-    # print('\nEnd of part 03.')
-    # press_continue()
 
 
 def ex03():
@@ -159,7 +138,6 @@ def ex06():
 
 def average(x, y):
     """Use to return the average value of the two parameters."""
-    # print('\nThis is exercise 07.\n')
     aver_value_07 = (x + y) / 2
     return aver_value_07
 
@@ -175,13 +153,11 @@ def ex07():
 
 def pretty_list(in_list_08):
     """Use for exercise 08."""
-    # print('\nThis is exercise 08.\n')
     if len(in_list_08) == 0:
         print('\nYour list is empty.')
     else:
         for i, index in enumerate(in_list_08):
             print(f'{i + 1}. {index}')
-    # print('\nEnd of part 08.')
 
 
 def ex08():
