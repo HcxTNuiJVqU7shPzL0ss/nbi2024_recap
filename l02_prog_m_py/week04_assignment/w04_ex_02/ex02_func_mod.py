4bc75391a62ec3ef65f3decc2e2f2d5ec6be1edd
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2025-12-30 15:08:12 +0100

    Refactor w04 ex02
    Move more functionality
    to functions, away from
    main

--- l02_prog_m_py/week04_assignment/w04_ex_02/ex02_func_mod.py
+++ l02_prog_m_py/week04_assignment/w04_ex_02/ex02_func_mod.py
@@ -22,25 +22,35 @@ Practice using functions and modules.
 
 
 # pylint: disable=import-error
-from ex02_functions import my_function_2_1
-from ex02_functions import eko_2a
-from ex02_functions import eko_2b
-from ex02_functions import my_function_2_3
-from ex02_functions import last
-from ex02_functions import cut_edges
-from ex02_functions import increase
-from ex02_functions import average
-from ex02_functions import pretty_list
+# from ex02_functions import my_function_2_1
+# from ex02_functions import eko_2a
+# from ex02_functions import eko_2b
+# from ex02_functions import my_function_2_3
+# from ex02_functions import last
+# from ex02_functions import cut_edges
+# from ex02_functions import increase
+# from ex02_functions import average
+# from ex02_functions import pretty_list
 # To get pylint to be happy, need to import one by one,
 # not use wildcard.
 # from ex02_functions import *
+from ex02_functions import ex01
+from ex02_functions import ex02a
+from ex02_functions import ex02b
+from ex02_functions import ex03
+from ex02_functions import ex04
+from ex02_functions import ex05
+from ex02_functions import ex06
+from ex02_functions import ex07
+from ex02_functions import ex08
 # pylint: disable=enable
 
 
-from my_funct_dir.my_base_functions import (press_exit,
-                                            press_continue,
-                                            enter_string,
-                                            enter_int_range)
+from my_funct_dir.my_base_functions import (press_continue)
+# (press_exit,
+#                                             press_continue))#,
+#                                             # enter_string,
+#                                             # enter_int_range)
 
 
 def main():
@@ -51,51 +61,60 @@ def main():
     press_continue()
 
     # Part 01
-    name_02_01 = 'What is your name: '
-    use_name_02_01 = enter_string(name_02_01)
-    my_function_2_1(use_name_02_01)
+    # name_02_01 = 'What is your name: '
+    # use_name_02_01 = enter_string(name_02_01)
+    # my_function_2_1(use_name_02_01)
+    ex01()
 
     # Part 02a
-    echo_string_2a = enter_string('Please enter what to echo: ')
-    eko_2a(echo_string_2a)
+    # echo_string_2a = enter_string('Please enter what to echo: ')
+    # eko_2a(echo_string_2a)
+    ex02a()
 
     # Part 02b
-    echo_string_2b = enter_string('Please enter what to echo: ')
-    cnt_string = 'Please enter how many times to echo: '
-    echo_cnt_2b = enter_int_range(cnt_string, 3, 10, True)
-    eko_2b(echo_string_2b, echo_cnt_2b)
+    # echo_string_2b = enter_string('Please enter what to echo: ')
+    # cnt_string = 'Please enter how many times to echo: '
+    # echo_cnt_2b = enter_int_range(cnt_string, 3, 10, True)
+    # eko_2b(echo_string_2b, echo_cnt_2b)
+    ex02b()
 
     # Part 03
-    my_function_2_3()
+    # my_function_2_3()
+    ex03()
 
     # Part 04
-    return_04 = last([1, 2, 3])
-    print(return_04)
-    print('\nEnd of part 04.')
-    press_continue()
+    # return_04 = last([1, 2, 3])
+    # print(return_04)
+    # print('\nEnd of part 04.')
+    # press_continue()
+    ex04()
 
     # Part 05
-    return_05 = cut_edges([0, 1, 2, 3])
-    print(return_05)
-    print('\nEnd of part 05.')
-    press_continue()
+    # return_05 = cut_edges([0, 1, 2, 3])
+    # print(return_05)
+    # print('\nEnd of part 05.')
+    # press_continue()
+    ex05()
 
     # Part 06
-    print(increase(1))
-    print('\nEnd of part 06.')
-    press_continue()
+    # print(increase(1))
+    # print('\nEnd of part 06.')
+    # press_continue()
+    ex06()
 
     # Part 07
-    result_07 = average(4, 8)
-    print(result_07)
-    print('\nEnd of part 07.')
-    press_continue()
+    # result_07 = average(4, 8)
+    # print(result_07)
+    # print('\nEnd of part 07.')
+    # press_continue()
+    ex07()
 
     # Part 08
-    pretty_list(['a', 'b', 3.14])
+    # pretty_list(['a', 'b', 3.14])
+    ex08()
 
 
-    press_exit()
+    # press_exit()
 
 
 if __name__ == "__main__":
--- l02_prog_m_py/week04_assignment/w04_ex_02/ex02_functions.py
+++ l02_prog_m_py/week04_assignment/w04_ex_02/ex02_functions.py
@@ -18,37 +18,70 @@
 #####################################################################
 
 
-from my_funct_dir.my_base_functions import (press_continue)
+from my_funct_dir.my_base_functions import (press_continue,
+                                            press_exit,
+                                            enter_string,
+                                            enter_int_range)
 
 
 def my_function_2_1(in_name):
     """Use for exercise 02, part 01."""
-    print('\nThis is exercise 01.\n')
+    # print('\nThis is exercise 01.\n')
     add_string = ' is a real hacker'
-    print(in_name + add_string)
+    print('\n' + in_name + add_string)
+    # print('\nEnd of part 01.')
+    # press_continue()
+
+
+def ex01():
+    """Use to run exercise 01."""
+    print('This is exercise 01.\n')
+    name_02_01 = 'What is your name: '
+    use_name_02_01 = enter_string(name_02_01)
+    my_function_2_1(use_name_02_01)
     print('\nEnd of part 01.')
     press_continue()
 
 
 def eko_2a(in_string):
     """Use to echo incoming string."""
-    print('\nThis is exercise 02a.\n')
+    # print('\nThis is exercise 02a.\n')
     print(in_string + in_string)
+    # print('\nEnd of part 02a.')
+    # press_continue()
+
+
+def ex02a():
+    """Use to run exercise 02a."""
+    print('This is exercise 02a.\n')
+    echo_string_2a = enter_string('Please enter what to echo: ')
+    eko_2a(echo_string_2a)
     print('\nEnd of part 02a.')
     press_continue()
 
 
 def eko_2b(in_string, count):
     """Use to echo incoming string count number of times."""
-    print('\nThis is exercise 02b.\n')
+    # print('\nThis is exercise 02b.\n')
     # int_cnt = count
     # int_string = in_string
-    print(in_string * count)
+    print('\n' + in_string * count)
     # int_cnt -= 1
     # while int_cnt > 0:
     #     int_string += in_string
     #     int_cnt -= 1
     # print(int_string)
+    # print('\nEnd of part 02b.')
+    # press_continue()
+
+
+def ex02b():
+    """Use to run exercise 02a."""
+    print('This is exercise 02b.\n')
+    echo_string_2b = enter_string('Please enter what to echo: ')
+    cnt_string = 'Please enter how many times to echo: '
+    echo_cnt_2b = enter_int_range(cnt_string, 3, 10, True)
+    eko_2b(echo_string_2b, echo_cnt_2b)
     print('\nEnd of part 02b.')
     press_continue()
 
@@ -56,7 +89,7 @@ def eko_2b(in_string, count):
 def my_function_2_3():
     """Use for exercise 02, part 03."""
     # Loop 5 times, should print out: 32.
-    print('\nThis is exercise 03.\n')
+    # print('\nThis is exercise 03.\n')
 
     end = 5
     y = 1
@@ -67,45 +100,93 @@ def my_function_2_3():
             break
     print(y)
 
+    # print('\nEnd of part 03.')
+    # press_continue()
+
+
+def ex03():
+    """Use to run exercise 03."""
+    print('This is exercise 03.\n')
+    my_function_2_3()
     print('\nEnd of part 03.')
     press_continue()
 
 
 def last(in_list_04):
     """Use to return the last element of the list parameter."""
-    print('\nThis is exercise 04.\n')
     return in_list_04[-1]
+
+
+def ex04():
+    """Use to run exercise 04."""
+    print('This is exercise 04.\n')
+    return_04 = last([1, 2, 3])
+    print(return_04)
+    print('\nEnd of part 04.')
+    press_continue()
 
 
 def cut_edges(in_list_05):
     """Use to return the list without the first and last element."""
-    print('\nThis is exercise 05.\n')
     internal_list = in_list_05
     internal_list.remove(0)
     internal_list.pop(-1)
     return internal_list
 
 
+def ex05():
+    """Use to run exercise 05."""
+    print('This is exercise 05.\n')
+    return_05 = cut_edges([0, 1, 2, 3])
+    print(return_05)
+    print('\nEnd of part 05.')
+    press_continue()
+
+
 def increase(x):
     """Use to increase x by 1 and return the result."""
-    print('\nThis is exercise 06.\n')
     x += 1
     return x
 
 
+def ex06():
+    """Use to run exercise 06."""
+    print('This is exercise 06.\n')
+    print(increase(1))
+    print('\nEnd of part 06.')
+    press_continue()
+
+
 def average(x, y):
     """Use to return the average value of the two parameters."""
-    print('\nThis is exercise 07.\n')
+    # print('\nThis is exercise 07.\n')
     aver_value_07 = (x + y) / 2
     return aver_value_07
 
 
+def ex07():
+    """Use to run exercise 07."""
+    print('This is exercise 07.\n')
+    result_07 = average(4, 8)
+    print(result_07)
+    print('\nEnd of part 07.')
+    press_continue()
+
+
 def pretty_list(in_list_08):
     """Use for exercise 08."""
-    print('\nThis is exercise 08.\n')
+    # print('\nThis is exercise 08.\n')
     if len(in_list_08) == 0:
         print('\nYour list is empty.')
     else:
         for i, index in enumerate(in_list_08):
             print(f'{i + 1}. {index}')
+    # print('\nEnd of part 08.')
+
+
+def ex08():
+    """Use to run exercise 07."""
+    print('This is exercise 08.\n')
+    pretty_list(['a', 'b', 3.14])
     print('\nEnd of part 08.')
+    press_exit()
