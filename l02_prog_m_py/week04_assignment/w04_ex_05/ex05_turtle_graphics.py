b53cff0033fd0a874ac796f507cecb6ed7d7a31c
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2026-01-03 19:13:08 +0100

    Part 3 and some for last
    Added two versions of code
    to handle part 3, draw a
    circle
    Also added the part to
    draw the capital P of
    PYTHON

--- l02_prog_m_py/week04_assignment/w04_ex_05/ex05_turtle_graphics.py
+++ l02_prog_m_py/week04_assignment/w04_ex_05/ex05_turtle_graphics.py
@@ -50,23 +50,87 @@ def move_next(space):
     turtle.forward(space)
 
 
+def draw_circle(radius):
+    """Use to draw a circle."""
+    # Ensure pen is down
+    turtle.pendown()
+    turtle.circle(radius)
+
+
+def alternate_part03(no_steps, draw_pix, angle):
+    """Alternate way of drawing a circle."""
+    # Ensure pen is down
+    turtle.pendown()
+    for x in range(no_steps):
+        turtle.forward(draw_pix)
+        turtle.right(angle)
+
+
+def draw_p(p_height, p_range, p_steps, p_angle):
+    """Use to draw the capital P in python."""
+    # Ensure pen is down
+    turtle.pendown()
+    # Point "upwards"
+    turtle.left(90)
+    # Draw the main leftmost line of P
+    turtle.forward(p_height)
+    # Draw the half circle of P
+    turtle.right(90)
+    alternate_part03(p_range, p_steps, p_angle)
+
+
 def main():
     """Use as main function."""
     print('\nWeek 04, Exercise 05, Turtle graphics.'
           '\nFunction: ',
           main.__name__, sep = '')
     screen = Screen()
+    press_continue()
+
+    turtle.speed(3)
+    #
+    # # Part 1: Function that draws a square with length of side
+    # # as parameter.
+    # side_square = 200
+    # draw_square(side_square)
+    # press_continue()
+    #
+    # # Part 2: Move cursor, then draw one more square.
+    # new_space = side_square / 2
+    # move_next(new_space)
+    # draw_square(side_square)
+    # press_continue()
+    #
+    # # Part 3: Draw a circle.
+    # c_radius = side_square / 2
+    # move_next(new_space * 2)
+    # draw_circle(c_radius)
+    # press_continue()
+    #
+    # # Alternate part 3
+    # # Ensure pen is up
+    # turtle.penup()
+    # turtle.clear()
+    # turtle.home()
+    # range_3 = 360
+    # forward_3 = 1
+    # right_3 = 1
+    # alternate_part03(range_3, forward_3, right_3)
     # press_continue()
 
-    # Exercise 1: Function that draws a square with length of side
-    # as parameter.
-    side_square = 200
-    draw_square(side_square)
 
-    # Exercise 2: Move cursor, then draw one more square
-    new_space = side_square / 2
-    move_next(new_space)
-    draw_square(side_square)
+    # Draw PYTHON
+
+    # Draw P
+    # Ensure pen is up
+    turtle.penup()
+    turtle.clear()
+    turtle.home()
+    height_p = 360
+    range_p = int(height_p / 2)
+    forward_p = 1.5
+    right_p = 1
+    draw_p(height_p, range_p, forward_p, right_p)
 
 
     # Allow the window to stay until user close it
