f1cf7b1dfe2a4ac3b4d555f408dda3f30db532bd
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2026-01-08 17:02:28 +0100

    Add L02, W05, Ex1.5
    Adding part 1.5
    Code and unit tests

---
+++ l02_prog_m_py/week05_assignment/w05_ex_01/tests/test_ex01_5_found_2nd_max.py
@@ -0,0 +1,78 @@
+"""Module for tests, L02, W05, Ex01.5."""
+
+#####################################################################
+# Copyright 2026 gnoff
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#####################################################################
+
+
+from ..ex01_5_find_2nd_max import (find_2nd_max)
+
+
+def test_find_2nd_max__empty_list():
+    """Used for unit test of function find_2nd_max.
+
+    Check empty list.
+    """
+    expected = None
+    actual = find_2nd_max([])
+    assert actual[0] == expected
+
+
+def test_find_2nd_max__one_element_list():
+    """Used for unit test of function find_2nd_max.
+
+    Check list with only one element.
+    """
+    expected = None
+    actual = find_2nd_max([100])
+    assert actual[0] == expected
+
+
+def test_find_2nd_max__number_list_max():
+    """Used for unit test of function find_2nd_max.
+
+    Check numbered list for second to max value.
+    """
+    assert find_2nd_max([1, 2, 3, 4, 5, -17, 'er'])[0] == 4
+    assert find_2nd_max([98.5, 1.5, 0, 'hi', 0.3])[0] == 1.5
+    assert find_2nd_max([-30, '32', 15, -99])[0] == -30
+    assert find_2nd_max([1, 2, 0.5])[0] == 1
+    assert find_2nd_max([15, 5, 4, 2, 1])[0] == 5
+    assert find_2nd_max([-1, 1, 'a', -34])[0] == -1
+    assert find_2nd_max([1.2, 1.8, 0.8])[0] == 1.2
+    assert find_2nd_max([5, 5, 5, 5, 9])[0] == 5
+    assert find_2nd_max([105, 5, -5985, 500, 9])[0] == 105
+    assert find_2nd_max([-999, 1.1])[0] == -999
+    assert find_2nd_max([999, 999])[0] == 999
+    assert find_2nd_max([999, 999, 5])[0] == 999
+
+    assert find_2nd_max([4, 1, 2, 3])[0] == 3
+    assert find_2nd_max([345, 3, 79])[0] == 79
+    assert find_2nd_max([100, 4, 4, -12])[0] == 4
+    assert find_2nd_max([-12, -13, -14])[0] == -13
+
+
+def test_find_2nd_max__non_numbered_list():
+    """Used for unit test of function find_2nd_max.
+
+    Check list including only non numbers.
+    """
+    expected = None
+    a = find_2nd_max([[], 'det', 'a'])
+    b = find_2nd_max(['hello'])
+    c = find_2nd_max([str(2), '234'])
+    assert b[0] == c[0]
+    assert a[0] == expected
