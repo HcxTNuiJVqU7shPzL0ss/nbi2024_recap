4edc8a3b18b20d6ab016e70d63a1f7e194397c8a
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2026-01-08 11:33:14 +0100

    We05 Ex01 part 1.3 added
    Adding part 1.3
    Code and tests
    a and b as one

---
+++ l02_prog_m_py/week05_assignment/w05_ex_01/ex01_3_count_vowels.py
@@ -0,0 +1,58 @@
+"""Module for Lesson 02, Week 05, Exercise 01.2, Count vowels.
+
+Update the code and fix it, including tests in separate file.
+"""
+
+#####################################################################
+# Copyright 2026 gnoff
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#####################################################################
+
+
+from my_funct_dir.my_base_functions import (press_continue,
+                                            press_exit, enter_string)
+
+
+def count_vowels(word_in):
+    """Count vowels in incoming word."""
+    check_vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+    vowel = 0
+    if not word_in:
+        return None
+    if isinstance(word_in, str):
+        for char in word_in:
+            if char.lower() in check_vowels:
+                vowel += 1
+    else:
+        vowel = 'incorrect'
+    return vowel
+
+
+def main():
+    """Use as main function."""
+    print('\nWeek 05, Exercise 01.3, Count vowels.\nFunction: ',
+          main.__name__, sep = '')
+    press_continue()
+
+    use_string = enter_string('What to count: ')
+
+    vowels = count_vowels(use_string)
+    print(f'\nYou had: {vowels} vowels.')
+
+    press_exit()
+
+
+if __name__ == "__main__":
+    main()
