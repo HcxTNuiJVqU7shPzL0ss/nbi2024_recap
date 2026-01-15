648ce83044e10881bc3bd61dbf3d5a66b7071fb3
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2026-01-15 17:14:26 +0100

    All also OG w01, Ex01
    Adding in, pretty much,
    the OG files from 2024 for
    exercise 01.
    Have to add some minor things
    as to allow pylint and
    pydocstyle to still run OK

---
+++ l02_prog_m_py/week01_assignment/w01_ex_01/og_2024/main.py
@@ -0,0 +1,21 @@
+# pylint: skip-file
+# noqa
+#####################################################################
+# Jan Berglund - main.py
+#
+# Veckouppgift 1
+# Vecka 2, 8/1
+# 1 Skapa projekt och synka med GitHub
+#
+# Programmering med Python, 20yhp
+#####################################################################
+
+
+# Name to use in printout
+name = "Jan Berglund"
+
+print("\nExercise 1")
+
+# Exercise is defined to type out two lines in terminal
+print("\nHello world")
+print("This program was made by " + name)
