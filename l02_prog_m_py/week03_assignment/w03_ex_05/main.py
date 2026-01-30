3b77340c6b224270a14723133c619a9182ff4fd8
rock_ <rock_gnoff@hotmail.com>
2026-01-30 11:49:09 +0100

    L02 W03 Ex5 done
    Coded added for exercise 5

--- l02_prog_m_py/week03_assignment/w03_ex_05/main.py
+++ l02_prog_m_py/week03_assignment/w03_ex_05/main.py
@@ -22,8 +22,56 @@ TAP HT 25D.
 #####################################################################
 
 
+import random
+
+
 from my_funct_dir.my_base_functions import (press_continue,
-                                            press_exit)
+                                            press_exit, enter_int_range)
+
+
+def generate_random_int():
+    """Use to generate a random integer."""
+    lowest_int = 1 # Lowest int that can be generated
+    highest_int = 100 # Highest int that can be generated
+
+    # Generate the random number based on lowest and highest allowed
+    random_int = random.randint(lowest_int, highest_int)
+
+    return [random_int, lowest_int, highest_int]
+
+
+def guess_the_number(secret):
+    """Use to guess the secret number."""
+    the_secret_one = secret[0]
+    lowest = secret[1]
+    highest = secret[2]
+    ask_str_number = 'Please enter your guess: '
+
+    no_of_guesses = 0
+
+    print(f'Welcome to guess the number!\n'
+          f'I am thinking about a number between {lowest} '
+          f'and {highest}.\n'
+          f'Can you guess which number it is?')
+    press_continue()
+
+    while True:
+        guess = enter_int_range(ask_str_number, lowest, highest,
+                                True)
+        no_of_guesses += 1
+        print('')
+        if guess == the_secret_one:
+            print(f'Your guess {guess} is correct!\n'
+                  f'You managed it in {no_of_guesses} guesses.')
+            break
+        if guess < the_secret_one:
+            print('No, sorry, that is too low!')
+        else:
+            print('No, sorry, that is too high!')
+        if abs(guess - the_secret_one) <= 5:
+            print('However, you are now getting close!')
+        press_continue()
+        continue
 
 
 def main():
@@ -35,6 +83,9 @@ def main():
           'from week 3.')
 
     press_continue()
+
+    secret_number = generate_random_int()
+    guess_the_number(secret_number)
 
     press_exit()
 
