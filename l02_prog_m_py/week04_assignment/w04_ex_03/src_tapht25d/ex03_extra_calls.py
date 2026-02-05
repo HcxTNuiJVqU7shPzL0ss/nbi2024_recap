8502ad84fa5c185414376246af453f39ce0266c3
HcxTNuiJVqU7shPzL0ss <jan.berglund@student.nbi-handelsakademin.se>
2026-02-05 17:31:29 +0100

    Adding L02 W04 Ex03 version 2
    Version 2 files added for
    exercise 03 or week 04
    code and test

--- l02_prog_m_py/week04_assignment/w04_ex_03/src_tapht25d/ex03_extra_calls.py
+++ l02_prog_m_py/week04_assignment/w04_ex_03/src_tapht25d/ex03_extra_calls.py
@@ -23,7 +23,7 @@ TAP HT 25D.
 
 
 # pylint: disable=import-error
-from ex03_functions import (ex03_01_find_number)
+from ex03_functions import (ex03_01_find_number, ex03_02_find_random)
 # pylint: enable=import-error
 
 
@@ -41,11 +41,35 @@ def ex03_version1_find_number():
     press_continue()
 
 
+def ex03_version2_find_random():
+    """Use to have random cards bring the total above 21.
+
+    Returns
+    Index 0: A list of the cards dealt
+    Index 1: The sum total (above 21)
+    Index 2: The number of cards delt
+    """
+    print('This is version 2 from exercise 03.\n'
+          'After random cards have brought the sum total above 21, '
+          'a few things will presented.')
+    press_continue()
+    returning_list = ex03_02_find_random()
+    print(f'The sum total was: {returning_list[1]}\n'
+          f'The number of cards were: {returning_list[2]}')
+    print('The cards in order were:')
+    for i in returning_list[0]:
+        print(i)
+    press_continue()
+
+
 def run_all_functions_03():
     """Use to run all the functions.
 
     Rather than cluttering the main file with function calls,
     will run all functions from here.
     """
-    # Part 1
+    # Version 1
     ex03_version1_find_number()
+
+    # Version 2
+    ex03_version2_find_random()
